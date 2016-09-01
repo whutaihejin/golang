78f8c25c3c0747b6f12d2f263e4de6b0b24371f7
taihejin <taihejin2008@126.com>
2016-09-01 20:07:17 +0800

    learning & fighting

--- chapter3/t3.5.go
+++ chapter3/t3.5.go
@@ -7,6 +7,21 @@ func main() {
 	fmt.Printf("len(s)=%d\n", len(s))
 	fmt.Println(s[0], s[7])
 	// panic
-	c := s[len(s)]
-	c++
+	// c := s[len(s)]
+	// c++
+	fmt.Println(s[0:5])
+	fmt.Println(s[:5])
+	fmt.Println(s[7:])
+	fmt.Println(s[:])
+
+	fmt.Println("goodbye" + s[5:])
+
+	v := "left root"
+	t := v
+	v += ", right foot"
+	fmt.Println(v)
+	fmt.Println(t)
+
+	// cannot assign to s[0]
+	// s[0] = 'L'
 }
