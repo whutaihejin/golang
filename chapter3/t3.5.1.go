608d0e030189c04b1a73cc1611222994ca320eeb
taihejin <taihejin2008@126.com>
2016-09-02 17:32:55 +0800

    learnig & fighting

--- chapter3/t3.5.1.go
+++ chapter3/t3.5.1.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	s := "Hello, 世界"
 	fmt.Println(s)
+	const GoUsage = `Go is a tool for managing Go source code.
+	
+	Usage:
+		go command [arguments]
+	...`
+	fmt.Println(GoUsage)
 }
