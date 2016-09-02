490fa96ce2398a9a1007db595c71577242b82463
taihejin <taihejin2008@126.com>
2016-09-02 20:11:50 +0800

    learning & fighting

---
+++ chapter3/t3.5.4.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+import "strings"
+
+func main() {
+	fmt.Printf("%s %s\n", "a/b/c.go", basename("a/b/c.go"))
+	fmt.Printf("%s %s\n", "c.d.go", basename("c.d.go"))
+	fmt.Printf("%s %s\n", "abc", basename("abc"))
+	fmt.Println(comma("1"))
+	fmt.Println(comma("12"))
+	fmt.Println(comma("123"))
+	fmt.Println(comma("1234"))
+	fmt.Println(comma("12345"))
+	fmt.Println(comma("123456"))
+	fmt.Println(comma("1234567"))
+}
+
+func basenameV1(s string) string {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
